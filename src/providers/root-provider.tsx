6a1e3107d713afa487485ef124ae1708383c9f06
Amir Salehi <salehi.amir9@gmail.com>
2025-08-13 14:27:56 +0530

    feat: add campaign selection provider to root providers

--- src/providers/root-provider.tsx
+++ src/providers/root-provider.tsx
@@ -1,8 +1,11 @@
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "../api/query-client";
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '../api/query-client';
+import { CampaignProvider } from './campaign-provider';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <CampaignProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </CampaignProvider>
   );
 }
