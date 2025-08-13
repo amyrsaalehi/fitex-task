83238bf209440bbc997ca0dbcf24d129610666f0
Amir Salehi <salehi.amir9@gmail.com>
2025-08-13 13:22:23 +0530

    feat: query client added

---
+++ src/providers/root-provider.tsx
@@ -0,0 +1,8 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "../api/query-client";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
