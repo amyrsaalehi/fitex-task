75abbd5ac441eafdfa0e1cf0f1659b864f249382
Amir Salehi <salehi.amir9@gmail.com>
2025-08-13 14:26:02 +0530

    feat: routes completed

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom';
 import AppLayout from './components/layout';
 import OverviewPage from './pages/overview-page';
+import CampaignsPage from './pages/campaign-page';
+import CreateCampaignPage from './pages/create-campaign-page';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    children: [{ index: true, element: <OverviewPage /> }],
+    children: [
+      { index: true, element: <OverviewPage /> },
+      { path: 'campaigns', element: <CampaignsPage /> },
+      { path: 'create', element: <CreateCampaignPage /> },
+    ],
   },
 ]);
